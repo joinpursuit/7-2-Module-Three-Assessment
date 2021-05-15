2a9aa9c6b5ced4167c6b28489e6b1c3630667920
Jordan Bobadilla <jordanboba08@gmail.com>
2021-05-15 10:22:02 -0400

    NavBar created

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import {NavLink} from "react-router-dom"
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <NavLink to={"/"}><img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh3H2SCuJBcmnrjvLMYrbWth4-_pUKiPUwdQ&usqp=CAU"}></img></NavLink>
+                <NavLink to={"/movies"}>Movies</NavLink>
+                <NavLink to={"/people"}>People</NavLink>
+                <NavLink to={"/locations"}>Locations</NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
