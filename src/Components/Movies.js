5fb245e1a8524818270d5e311395fac708c2bde4
Marsha Hall <mphall77@gmail.com>
2021-05-15 10:52:39 -0400

    create movie dropdowm

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -2,9 +2,40 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Movies = () => {
+	// declare useState
+	const [movies, setMovies] = useState([]);
+
+	// function to call the API
+	const fetchMovies = async () => {
+		try {
+			const res = await axios.get("https://ghibliapi.herokuapp.com/films");
+
+			setMovies(res.data);
+		} catch (err) {
+			setMovies([]);
+		}
+	};
+
+	// call the function for the API
+	useEffect(() => {
+		fetchMovies();
+	}, []);
+	// get the option selected
 	return (
 		<section>
 			<h1>Select a Movie</h1>
+
+			<select>
+				<option value=""></option>
+				{/* map the movies array */}
+				{movies.map((movie) => {
+					return (
+						<option key={movie.id} value={movie.url}>
+							{movie.title}
+						</option>
+					);
+				})}
+			</select>
 		</section>
 	);
 };
