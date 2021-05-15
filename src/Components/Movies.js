1fb288dc0038e9977ce5ba6c977a69abd1d10682
Coreen Cooper <coreencooper@pursuit.org>
2021-05-15 11:35:59 -0400

    create dropdown menu

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,9 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Movies = () => {
-    return(
-        <section id="Movies">
-            <h1>Movies</h1>
-        </section>
-    )
-}
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    try {
+      const res = await axios.get("https://ghibliapi.herokuapp.com/films/");
+      setMovies(res.data);
+    } catch (error) {
+      console.log(error);
+      setMovies([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <section id="Movies">
+      <h1>Select a Movie</h1>
+      <select>
+        <option value=""></option>
+        {movies.map((movie) => {
+          return (
+            <option value={movie.id} key={movie.id}>
+              {movie.title}
+            </option>
+          );
+        })}
+        <option />
+      </select>
+    </section>
+  );
+};
 
 export default Movies;
