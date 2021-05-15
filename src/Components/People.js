677886647e608cc27e5184662f1fd297293eb736
Coreen Cooper <coreencooper@pursuit.org>
2021-05-15 17:50:20 -0400

    update People.js

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./People.css"
-
-
+import "./People.css";
 
 const People = () => {
   const [people, setPeople] = useState([]);
@@ -16,15 +14,15 @@ const People = () => {
     try {
       const res = await axios.get("https://ghibliapi.herokuapp.com/people");
       setPeople(res.data);
-      // res.data.forEach(data => {
-      //   if(data[0] === )
-
-      // })
     } catch (error) {
       console.log(error);
       setPeople([]);
     }
   };
+
+  const getPeopleInfo = people.filter((person) => {
+    return person.name === userInput;
+  });
 
   const handleChange = (e) => {
     // if (e.target.value === "") {
@@ -32,37 +30,36 @@ const People = () => {
     //   setGender(null);
     //   setAge(null);
     // } else {
-      setUserInput(e.target.value);
+    setUserInput(e.target.value);
     // }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setPerson(userInput);
+    setAge(getPeopleInfo[0].age);
+    setGender(getPeopleInfo[0].gender);
   };
 
-  const getPeopleInfo = people.filter((person) => {
-    return person.name === userInput;
-  });
+  // const handleClick = () => {
+  //   // if (getPeopleInfo !== undefined) {
+  //     // console.log(getPeopleInfo)
+  //     // console.log(getPeopleInfo[0])
+  //     // console.log(getPeopleInfo[0].age)
+  //     setUserInput("");
+  //     setPerson(userInput);
+  //     setAge(getPeopleInfo[0].age);
+  //     setGender(getPeopleInfo[0].gender);
 
-  const handleClick = () => {
-    if (getPeopleInfo !== undefined) {
-      // console.log(getPeopleInfo)
-      // console.log(getPeopleInfo[0])
-      // console.log(getPeopleInfo[0].age)
-      setUserInput("");
-      setPerson(userInput);
-      setAge(getPeopleInfo[0].age);
-      setGender(getPeopleInfo[0].gender);
-    } else {
-      return(
-        "Not Found"
+  //     // return(
+  //     //   "Not Found"
 
-      )
-      // setPerson(null);
-      // setAge(null);
-      // setGender(null);
-    }
-  };
+  //     // )
+  //     // setPerson(null);
+  //     // setAge(null);
+  //     // setGender(null);
+  //   // }
+  // };
 
   useEffect(() => {
     fetchPeople();
@@ -80,12 +77,17 @@ const People = () => {
             placeholder="Find Your Person"
           />
         </label>
-      <button onClick={handleClick}>Submit</button>
+        <button>Submit</button>
       </form>
-      <h2>{alert}</h2>
-      <h2>Name: {person}</h2>
-      <h3>Age: {age}</h3>
-      <h3>Gender: {gender}</h3>
+      {getPeopleInfo.length === 0 ? (
+        <h2>Not Found</h2>
+      ) : (
+        <>
+          <h2>Name: {person}</h2>
+          <h3>Age: {age}</h3>
+          <h3>Gender: {gender}</h3>
+        </>
+      )}
     </section>
   );
 };
