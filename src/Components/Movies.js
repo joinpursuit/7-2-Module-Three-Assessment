329626fc8a08d53666214a9f45cade44236346ab
Marsha Hall <mphall77@gmail.com>
2021-05-15 11:26:15 -0400

    show movie details and clear on blank

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -4,9 +4,14 @@ import { useState, useEffect } from "react";
 const Movies = () => {
 	// declare useState
 	const [movies, setMovies] = useState([]);
+	const [selectedMovieURL, setSelectedMovieURL] = useState("");
+	const [selectedMovie, setSelectedMovie] = useState({});
+	const [hasSelected, setHasSelected] = useState(false);
+	let movieDetails;
 
 	// function to call the API
 	const fetchMovies = async () => {
+		setHasSelected(true);
 		try {
 			const res = await axios.get("https://ghibliapi.herokuapp.com/films");
 
@@ -20,12 +25,41 @@ const Movies = () => {
 	useEffect(() => {
 		fetchMovies();
 	}, []);
-	// get the option selected
+
+	// get the option selected ... base for movie details
+	const selectMovie = async (e) => {
+		setSelectedMovieURL(e.target.value);
+		try {
+			const res = await axios.get(e.target.value);
+			setSelectedMovie(res.data);
+		} catch (err) {
+			setSelectedMovie({});
+		}
+	};
+
+	{
+		/* print nothing if selected is blank */
+	}
+	if (hasSelected) {
+		if (selectedMovie.title) {
+			movieDetails = (
+				<div>
+					{/* access movie details object */}
+					<p>Title: {selectedMovie.title}</p>
+					<p>Release Date: {selectedMovie.release_date}</p>
+					<p>Description: {selectedMovie.description}</p>
+				</div>
+			);
+		}
+	} else {
+		movieDetails = null;
+	}
+
 	return (
 		<section>
 			<h1>Select a Movie</h1>
-
-			<select>
+			{/* pass movie selected to a function to get movie details */}
+			<select value={selectedMovieURL} onChange={selectMovie}>
 				<option value=""></option>
 				{/* map the movies array */}
 				{movies.map((movie) => {
@@ -36,6 +70,7 @@ const Movies = () => {
 					);
 				})}
 			</select>
+			{movieDetails}
 		</section>
 	);
 };
