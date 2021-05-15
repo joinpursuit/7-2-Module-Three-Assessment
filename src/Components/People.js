d694241dfbecebb5c2d0937bac01c9d3a8fa6629
Coreen Cooper <coreencooper@pursuit.org>
2021-05-15 17:26:11 -0400

    People.js

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,41 +1,91 @@
-// import axios from "axios";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./People.css"
+
+
+
 const People = () => {
+  const [people, setPeople] = useState([]);
   const [userInput, setUserInput] = useState("");
+  const [person, setPerson] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [alert, setAlert] = useState("");
 
-//   const fetchPersonInfo = async () => {
-//     try {
-//       const res = await axios.get("https://ghibliapi.herokuapp.com/people");
-//       debugger;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+  const fetchPeople = async () => {
+    try {
+      const res = await axios.get("https://ghibliapi.herokuapp.com/people");
+      setPeople(res.data);
+      // res.data.forEach(data => {
+      //   if(data[0] === )
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-
-    // debugger
+      // })
+    } catch (error) {
+      console.log(error);
+      setPeople([]);
+    }
   };
 
   const handleChange = (e) => {
-    setUserInput(e.target.value);
+    // if (e.target.value === "") {
+    //   setUserInput(null);
+    //   setGender(null);
+    //   setAge(null);
+    // } else {
+      setUserInput(e.target.value);
+    // }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const getPeopleInfo = people.filter((person) => {
+    return person.name === userInput;
+  });
+
+  const handleClick = () => {
+    if (getPeopleInfo !== undefined) {
+      // console.log(getPeopleInfo)
+      // console.log(getPeopleInfo[0])
+      // console.log(getPeopleInfo[0].age)
+      setUserInput("");
+      setPerson(userInput);
+      setAge(getPeopleInfo[0].age);
+      setGender(getPeopleInfo[0].gender);
+    } else {
+      return(
+        "Not Found"
+
+      )
+      // setPerson(null);
+      // setAge(null);
+      // setGender(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchPeople();
+  }, []);
 
   return (
     <section id="People">
       <form onSubmit={handleSubmit}>
         <label>
           <h1>Search for a Person</h1>
-          <input onChange={handleChange} type="text" />
+          <input
+            value={userInput}
+            onChange={handleChange}
+            type="text"
+            placeholder="Find Your Person"
+          />
         </label>
+      <button onClick={handleClick}>Submit</button>
       </form>
-      <button>Search</button>
-      <p>{userInput}</p>
-      <h2>name</h2>
-      <h3>age</h3>
-      <h3>gender</h3>
+      <h2>{alert}</h2>
+      <h2>Name: {person}</h2>
+      <h3>Age: {age}</h3>
+      <h3>Gender: {gender}</h3>
     </section>
   );
 };
